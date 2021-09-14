26d340f4a8d0bd376bbc12afdbb638b6a2a04b5c
Jesus Banuelos <jesus.banuelos.951@my.csun.edu>
2021-09-14 12:57:03 -0700

    Initial commit

---
+++ app/src/main/java/com/example/computerstarter/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.computerstarter;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.computerstarter.LoginFragment;
+import com.example.computerstarter.MainPageFragment;
+import com.example.computerstarter.QuizFragment;
+import com.example.computerstarter.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        // as soon as the application opens the first
+        // fragment should be shown to the user
+        // in this case it is algorithm fragment
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new QuizFragment()).commit();
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            // By using switch we can easily get
+            // the selected fragment
+            // by using there id.
+            Fragment selectedFragment = null;
+            switch (item.getItemId()) {
+                case R.id.quiz:
+                    selectedFragment = new QuizFragment();
+                    break;
+                case R.id.main_page:
+                    selectedFragment = new MainPageFragment();
+                    break;
+                case R.id.profile:
+                    selectedFragment = new LoginFragment();
+                    break;
+            }
+            // It will help to replace the
+            // one fragment to other.
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, selectedFragment)
+                    .commit();
+            return true;
+        }
+    };
+}
