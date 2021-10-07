1c66a9439a9e5a1d573a9d51a6dc43ca93ab43a9
jjb4547 <jesus.banuelos.951@my.csun.edu>
2021-10-07 14:04:29 -0700

    "COMST-45 Get a Settings Page Working" Got a basic layout for now.

--- app/src/main/java/com/example/computerstarter/SettingsActivity.java
+++ app/src/main/java/com/example/computerstarter/SettingsActivity.java
@@ -1,12 +1,23 @@
 package com.example.computerstarter;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceManager;
+
+import java.util.ArrayList;
+import java.util.Objects;
 
 public class SettingsActivity extends AppCompatActivity {
     RelativeLayout layout;
@@ -15,15 +26,20 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_layout);
         layout = findViewById(R.id.relativelayout);
-        getSupportActionBar().setTitle("Settings");
+        Objects.requireNonNull(getSupportActionBar()).setTitle("Settings");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        layout.setOnTouchListener(new OnSwipeTouchListener(SettingsActivity.this){
-            @Override
-            public void onSwipeRight(){
-                super.onSwipeRight();
-                finish();
+        if(findViewById(R.id.idFrameLayout)!=null){
+            if(savedInstanceState!=null){
+                return;
             }
-        });
+            getFragmentManager().beginTransaction().add(R.id.idFrameLayout, new SettingsFragment()).commit();
+        }
+        loadSettings();
+    }
+    private void loadSettings(){
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        boolean notifications = sp.getBoolean("notifications",false);
+        int volume = sp.getInt("volume_notifications",0);
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
