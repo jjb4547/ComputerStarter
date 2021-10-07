d9e563ba75d08b42c1ad51c1c548fb182ffd9e7b
BigHaircut <ryansan3337@gmail.com>
2021-10-07 13:47:40 -0700

    "COMST-40: Get a education layout" Changed the layout to use a listview instead of buttons The activity page the items will lead to is still work in progress

--- app/src/main/java/com/example/computerstarter/EducationActivities.java
+++ app/src/main/java/com/example/computerstarter/EducationActivities.java
@@ -3,8 +3,18 @@ package com.example.computerstarter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class EducationActivities extends AppCompatActivity {
+    //gonna try to make this like quiz activity
+    // for the title, paragraph and image
+    //private TextView eduTitle,eduPara;
+    //private ImageView eduPic;
+    //arraylist of display options
+    //private ArrayList
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
