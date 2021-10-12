1b4ded4c52a01dbae9571b946706e5495e2a5054
jjb4547 <jesus.banuelos.951@my.csun.edu>
2021-10-12 13:20:50 -0700

    "COMST-45 Get a Settings Page Working" Put something on the gradle

--- app/src/main/java/com/example/computerstarter/MainActivity.java
+++ app/src/main/java/com/example/computerstarter/MainActivity.java
@@ -3,8 +3,17 @@ package com.example.computerstarter;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.fragment.app.Fragment;
 
+import android.app.AlarmManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.MenuItem;
@@ -17,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //createNotificationChannel();
+        //addNotification();
         //Toolbar toolbar = findViewById(R.id.toolbar);
         //setSupportActionBar(toolbar);
 
@@ -27,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
         // fragment should be shown to the user
         // in this case it is algorithm fragment
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MainPageFragment()).commit();
+
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -55,4 +67,35 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
     };
+    /*private void addNotification(){
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this,"not_cs")
+                .setSmallIcon(R.mipmap.ic_logo)
+                .setContentTitle("Computer Starter Notification")
+                .setContentText("Come back we miss you!")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        //NotificationManagerCompat notificationManager = NotificationManagerCompat.from(getApplicationContext());
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_UPDATE_CURRENT);
+        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+
+        long time = System.currentTimeMillis();
+        long tenseconds = 1000*20;
+        alarmManager.set(alarmManager.RTC_WAKEUP,time+tenseconds,pendingIntent);
+        builder.setContentIntent(pendingIntent);
+
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.notify(0, builder.build());
+    }
+    private void createNotificationChannel(){
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+            CharSequence name = "ComputerStarterNoti_1";
+            String description = "Time off app";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("not_cs",name,importance);
+            channel.setDescription(description);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }*/
+
 }
