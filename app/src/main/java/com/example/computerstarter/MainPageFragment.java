43f3eae3b407dae38a37aa5318f1f4411b0b2130
jjb4547 <jesus.banuelos.951@my.csun.edu>
2021-11-16 12:53:37 -0800

    Just Getting new Merge

--- app/src/main/java/com/example/computerstarter/MainPageFragment.java
+++ app/src/main/java/com/example/computerstarter/MainPageFragment.java
@@ -1,27 +1,16 @@
 package com.example.computerstarter;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
-import com.getkeepsafe.taptargetview.TapTarget;
-import com.getkeepsafe.taptargetview.TapTargetView;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainPageFragment extends Fragment {
