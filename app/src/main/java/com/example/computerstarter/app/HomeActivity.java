c5c5ee46b66d2961e8f376c45d547f9a89c20f7e
Jesus_Mac <jesus.banuelos.951@my.csun.edu>
2022-03-09 21:07:04 -0800

    Added a onItemClickListener for the search button

--- app/src/main/java/com/example/computerstarter/app/HomeActivity.java
+++ app/src/main/java/com/example/computerstarter/app/HomeActivity.java
@@ -129,72 +129,130 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
             startActivity(new Intent(HomeActivity.this, MainBuilds.class)
                     .putExtra("from","Social"));
         });
-        searchParts.setOnKeyListener(new View.OnKeyListener() {
-            @Override
-            public boolean onKey(View v, int keyCode, KeyEvent event) {
-                if((event.getAction()==KeyEvent.ACTION_DOWN)&&(keyCode==KeyEvent.KEYCODE_ENTER)){
-                    switch (searchParts.getText().toString().toLowerCase()) {
-                        case "cpu":
-                            startActivity(new Intent(HomeActivity.this, Education_Choosing_Activity.class)
-                                    .putExtra("component", diffTitles[0])
-                                    .putExtra("Act", "Edu"));
-                            break;
-                        case "motherboard":
-                            startActivity(new Intent(HomeActivity.this, Education_Choosing_Activity.class)
-                                    .putExtra("component", diffTitles[1])
-                                    .putExtra("Act", "Edu"));
-                            break;
-                        case "memory":
-                            startActivity(new Intent(HomeActivity.this, Education_Choosing_Activity.class)
-                                    .putExtra("component", diffTitles[2])
-                                    .putExtra("Act", "Edu"));
-                            break;
-                        case "storage":
-                        case "hdd":
-                        case "hard drive":
-                        case "solid state":
-                        case "ssd":
-                            startActivity(new Intent(HomeActivity.this, Education_Choosing_Activity.class)
-                                    .putExtra("component", diffTitles[3])
-                                    .putExtra("Act", "Edu"));
-                            break;
-                        case "power supply":
-                        case "psu":
-                        case "power supply unit":
-                            startActivity(new Intent(HomeActivity.this, Education_Choosing_Activity.class)
-                                    .putExtra("component", diffTitles[4])
-                                    .putExtra("Act", "Edu"));
-                            break;
-                        case "cpu cooler":
-                        case "cooler":
-                            startActivity(new Intent(HomeActivity.this, Education_Choosing_Activity.class)
-                                    .putExtra("component", diffTitles[5])
-                                    .putExtra("Act", "Edu"));
-                            break;
-                        case "monitor":
-                            startActivity(new Intent(HomeActivity.this, Education_Choosing_Activity.class)
-                                    .putExtra("component", diffTitles[6])
-                                    .putExtra("Act", "Edu"));
-                            break;
-                        case "video card":
-                        case "gpu":
-                        case "vga":
-                        case "graphics card":
-                        case "graphic card":
-                            startActivity(new Intent(HomeActivity.this, Education_Choosing_Activity.class)
-                                    .putExtra("component", diffTitles[7])
-                                    .putExtra("Act", "Edu"));
-                            break;
-                        case "case":
-                        case "pc case":
-                            startActivity(new Intent(HomeActivity.this, Education_Choosing_Activity.class)
-                                    .putExtra("component", diffTitles[8])
-                                    .putExtra("Act", "Edu"));
-                            break;
-                    }
-                }
-                return true;
+        searchParts.setOnItemClickListener((parent, view, position, id) -> {
+            switch (partsItemList.get(position).getPartsName().toLowerCase()) {
+                case "cpu":
+                    startActivity(new Intent(HomeActivity.this, Education_Choosing_Activity.class)
+                            .putExtra("component", diffTitles[0])
+                            .putExtra("Act", "Edu"));
+                    break;
+                case "motherboard":
+                    startActivity(new Intent(HomeActivity.this, Education_Choosing_Activity.class)
+                            .putExtra("component", diffTitles[1])
+                            .putExtra("Act", "Edu"));
+                    break;
+                case "memory":
+                    startActivity(new Intent(HomeActivity.this, Education_Choosing_Activity.class)
+                            .putExtra("component", diffTitles[2])
+                            .putExtra("Act", "Edu"));
+                    break;
+                case "storage":
+                case "hdd":
+                case "hard drive":
+                case "solid state":
+                case "ssd":
+                    startActivity(new Intent(HomeActivity.this, Education_Choosing_Activity.class)
+                            .putExtra("component", diffTitles[3])
+                            .putExtra("Act", "Edu"));
+                    break;
+                case "power supply":
+                case "psu":
+                case "power supply unit":
+                    startActivity(new Intent(HomeActivity.this, Education_Choosing_Activity.class)
+                            .putExtra("component", diffTitles[4])
+                            .putExtra("Act", "Edu"));
+                    break;
+                case "cpu cooler":
+                case "cooler":
+                    startActivity(new Intent(HomeActivity.this, Education_Choosing_Activity.class)
+                            .putExtra("component", diffTitles[5])
+                            .putExtra("Act", "Edu"));
+                    break;
+                case "monitor":
+                    startActivity(new Intent(HomeActivity.this, Education_Choosing_Activity.class)
+                            .putExtra("component", diffTitles[6])
+                            .putExtra("Act", "Edu"));
+                    break;
+                case "video card":
+                case "gpu":
+                case "vga":
+                case "graphics card":
+                case "graphic card":
+                    startActivity(new Intent(HomeActivity.this, Education_Choosing_Activity.class)
+                            .putExtra("component", diffTitles[7])
+                            .putExtra("Act", "Edu"));
+                    break;
+                case "case":
+                case "pc case":
+                    startActivity(new Intent(HomeActivity.this, Education_Choosing_Activity.class)
+                            .putExtra("component", diffTitles[8])
+                            .putExtra("Act", "Edu"));
+                    break;
             }
+        });
+        searchParts.setOnKeyListener((v, keyCode, event) -> {
+            if((event.getAction()==KeyEvent.ACTION_DOWN)&&(keyCode==KeyEvent.KEYCODE_ENTER)){
+                switch (searchParts.getText().toString().toLowerCase()) {
+                    case "cpu":
+                        startActivity(new Intent(HomeActivity.this, Education_Choosing_Activity.class)
+                                .putExtra("component", diffTitles[0])
+                                .putExtra("Act", "Edu"));
+                        break;
+                    case "motherboard":
+                        startActivity(new Intent(HomeActivity.this, Education_Choosing_Activity.class)
+                                .putExtra("component", diffTitles[1])
+                                .putExtra("Act", "Edu"));
+                        break;
+                    case "memory":
+                        startActivity(new Intent(HomeActivity.this, Education_Choosing_Activity.class)
+                                .putExtra("component", diffTitles[2])
+                                .putExtra("Act", "Edu"));
+                        break;
+                    case "storage":
+                    case "hdd":
+                    case "hard drive":
+                    case "solid state":
+                    case "ssd":
+                        startActivity(new Intent(HomeActivity.this, Education_Choosing_Activity.class)
+                                .putExtra("component", diffTitles[3])
+                                .putExtra("Act", "Edu"));
+                        break;
+                    case "power supply":
+                    case "psu":
+                    case "power supply unit":
+                        startActivity(new Intent(HomeActivity.this, Education_Choosing_Activity.class)
+                                .putExtra("component", diffTitles[4])
+                                .putExtra("Act", "Edu"));
+                        break;
+                    case "cpu cooler":
+                    case "cooler":
+                        startActivity(new Intent(HomeActivity.this, Education_Choosing_Activity.class)
+                                .putExtra("component", diffTitles[5])
+                                .putExtra("Act", "Edu"));
+                        break;
+                    case "monitor":
+                        startActivity(new Intent(HomeActivity.this, Education_Choosing_Activity.class)
+                                .putExtra("component", diffTitles[6])
+                                .putExtra("Act", "Edu"));
+                        break;
+                    case "video card":
+                    case "gpu":
+                    case "vga":
+                    case "graphics card":
+                    case "graphic card":
+                        startActivity(new Intent(HomeActivity.this, Education_Choosing_Activity.class)
+                                .putExtra("component", diffTitles[7])
+                                .putExtra("Act", "Edu"));
+                        break;
+                    case "case":
+                    case "pc case":
+                        startActivity(new Intent(HomeActivity.this, Education_Choosing_Activity.class)
+                                .putExtra("component", diffTitles[8])
+                                .putExtra("Act", "Edu"));
+                        break;
+                }
+            }
+            return true;
         });
         searchButton.setOnClickListener(v->{
             if(searchParts.getText().toString().toLowerCase().equals("cpu")) {
